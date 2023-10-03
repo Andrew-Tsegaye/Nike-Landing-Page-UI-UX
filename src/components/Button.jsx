43b21eb9e9e73664817bc0d4072bee24454116c5
Andrew Tsegaye <andrewtsegaye7@gmail.com>
2023-10-04 02:33:27 +0300

    fixed bugs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,32 @@
-const Button = ({lable, iconUrl, backgroundColor, textColor, borderColor}) => {
+const Button = ({
+  lable,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
-     ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` :
-     `bg-coral-red text-white border-coral-red`
-     } rounded-full
-     `}>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+     ${
+       backgroundColor
+         ? `${backgroundColor} ${textColor} ${borderColor}`
+         : `bg-coral-red text-white border-coral-red`
+     } rounded-full ${fullWidth && "w-full"}
+     `}
+    >
       {lable}
 
-      {iconUrl && <img src={iconUrl} alt="ArrowRight" className="ml-2 rounded-full w-5 h-5"/>}
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="ArrowRight"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
